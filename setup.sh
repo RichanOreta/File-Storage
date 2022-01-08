020baa49ec073952f2d686cdeb476b1502a8b532
RichanOreta <96275478+RichanOreta@users.noreply.github.com>
2022-01-08 16:55:42 +0800

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -26,6 +26,5 @@ doas cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 doas reflector --fastest 15 --latest 15 --score 15 --sort rate --save /etc/pacman.d/mirrorlist
 
 yay --sudo doas --save
-yay -S --needed - < packages.txt --cleanafter
 
 cat packages.txt | xargs yay -S --needed --cleanafter
