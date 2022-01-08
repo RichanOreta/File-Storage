6bb1ee7e58e6129771ea17b905130d24f6cad8bd
Richan-Oreta <richanmoreta@gmail.com>
2022-01-08 15:02:41 +0800

    added packages.txt and setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+curl -o ~/.config/alacritty/alacritty.yml --create-dirs https://raw.githubusercontent.com/RichanOreta/File-Storage/master/dotfiles/alacritty/alacritty.yml
+
+curl -o ~/.config/i3/config --create-dirs https://raw.githubusercontent.com/RichanOreta/File-Storage/master/dotfiles/i3/config
+
+curl -o ~/.config/lf/lfrc --create-dirs https://raw.githubusercontent.com/RichanOreta/File-Storage/master/dotfiles/lf/lfrc
+
+curl -o ~/.config/nvim/init.vim --create-dirs https://raw.githubusercontent.com/RichanOreta/File-Storage/master/dotfiles/nvim/init.vim
+
+curl -o ~/.config/polybar/config --create-dirs https://raw.githubusercontent.com/RichanOreta/File-Storage/master/dotfiles/polybar/config
+
+curl -o ~/.config/polybar/launch.sh --create-dirs https://raw.githubusercontent.com/RichanOreta/File-Storage/master/dotfiles/polybar/launch.sh
